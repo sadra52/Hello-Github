c45ccaa6aae83414d1f7b12bb3af767e26e2941a
sadra52 <sadraddin1375@gmail.com>
2023-07-28 22:09:34 +0330

    Create HelloGithub.java

---
+++ HelloGithub.java
@@ -0,0 +1,9 @@
+public class Github {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello Github!");
+    }
+
+}
